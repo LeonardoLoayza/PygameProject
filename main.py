9da3cc7b55e26b3b2ea4f6ffd60d2e5ca738ab0e
Leonardo Loayza <leonardoloayza28@gmail.com>
2024-06-17 11:56:09 -0500

    agregando la muertey lava

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from pygame.locals import *
 pygame.init() 
 
 clock = pygame.time.Clock()
-fps = 60
+fps = 30
 
 screen_width = 1000  # 1000
 screen_height = 1000
@@ -13,10 +13,13 @@ screen_height = 1000
 screen = pygame.display.set_mode((screen_width, screen_height))
 
 tile_size = 50
+game_over = 0 
 
 # Cargar las imagenes
-sun_img = pygame.image.load('img/sun.png')
-bg_img = pygame.image.load('img/sky.png')
+# sun_img = pygame.image.load('img/sun.png')
+# bg_img = pygame.image.load('img/sky.png')
+# bg_img = pygame.image.load('img2/atardecer_00.png')
+bg_img = pygame.transform.scale(pygame.image.load("img2/atardcerme_0.jpg"), (screen_height, screen_width))
 
 jumping_surface = pygame.transform.scale(pygame.image.load("mario_jumping.png"), (48,64))
 
@@ -48,88 +51,98 @@ class Player():
     self.vel_y = 0
     self.jumped = False 
     
-  def update(self): 
-    dx = 0 
-    dy = 0 
+  def update(self, game_over):
+    dx = 0
+    dy = 0
     walk_cooldown = 5
     
-    # Mecanica de saltar, movimiento
-    key = pygame.key.get_pressed()
-    # Prevenir que pueda saltar infinitas veces
-    if key[pygame.K_SPACE] and self.jumped == False:
-      self.vel_y = -15
-      self.jumped = True
-    if key[pygame.K_SPACE] == False:
-     self.jumped = False 
-    # Cada tic se incrementa el counter y se actualiza la posicion del personaje
-    # A que lado va (izquierda o derecha)
-    if key[pygame.K_LEFT]:
-      dx -= 5
-      self.counter += 1
-      self.direction = -1
-    if key[pygame.K_RIGHT]:
-      dx += 5   
-      self.counter += 1
-      self.direction = 1
-    # Cuando se para, se pone la animacion inicial (IDLE)
-    if key[pygame.K_LEFT] == False and key[pygame.K_RIGHT] == False:
-      self.index = 0 
-      self.counter = 0 
-      # Revisar que lado el personaje esta viendo y cargar los sprites
-      if self.direction == 1: 
-        self.image = self.images_right[self.index]
-      if self.direction == -1:
-        self.image = self.images_left[self.index]
+    if game_over == 0:  
+      # Mecanica de saltar, movimiento
+      key = pygame.key.get_pressed()
+      # Prevenir que pueda saltar infinitas veces
+      if key[pygame.K_SPACE] and self.jumped == False:
+        self.vel_y = -15
+        self.jumped = True
+      if key[pygame.K_SPACE] == False:
+        self.jumped = False 
+      # Cada tic se incrementa el counter y se actualiza la posicion del personaje
+      # A que lado va (izquierda o derecha)
+      if key[pygame.K_LEFT]:
+        dx -= 5
+        self.counter += 1
+        self.direction = -1
+      if key[pygame.K_RIGHT]:
+        dx += 5   
+        self.counter += 1
+        self.direction = 1
+      # Cuando se para, se pone la animacion inicial (IDLE)
+      if key[pygame.K_LEFT] == False and key[pygame.K_RIGHT] == False:
+        self.index = 0 
+        self.counter = 0 
+        # Revisar que lado el personaje esta viendo y cargar los sprites
+        if self.direction == 1: 
+          self.image = self.images_right[self.index]
+        if self.direction == -1:
+          self.image = self.images_left[self.index]
 
-      
-    # Animacion 
-    # AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
-    if self.counter > walk_cooldown:
-      self.counter = 0
-      self.index += 1
-      if self.index >= len(self.images_right):
-        self.index = 0
-      if self.direction == 1: 
-        self.image = self.images_right[self.index]
-      if self.direction == -1:
-        self.image = self.images_left[self.index]
+      # Animacion 
+      # AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+      if self.counter > walk_cooldown:
+        self.counter = 0
+        self.index += 1
+        if self.index >= len(self.images_right):
+          self.index = 0
+        if self.direction == 1: 
+          self.image = self.images_right[self.index]
+        if self.direction == -1:
+          self.image = self.images_left[self.index]
 
-    # Gravedad, no exceder 10   
-    self.vel_y += 1
-    if self.vel_y > 10:
-      self.vel_y = 10 
-    dy += self.vel_y 
-    
-    # Verificar colision
-    for tile in world.tile_list: 
-      # Colision en X
-      if tile[1].colliderect(self.rect.x + dx, self.rect.y, self.width, self.height):
-        dx = 0
+      # Gravedad, no exceder 10   
+      self.vel_y += 1
+      if self.vel_y > 10:
+        self.vel_y = 10 
+      dy += self.vel_y 
       
-      # Si colisiona con algo en Y 
-      if tile[1].colliderect(self.rect.x, self.rect.y + dy, self.width, self.height):
-        # Verificar si esta debajo del suelo 
-        if self.vel_y < 0: 
-          dy = tile[1].bottom - self.rect.top
-          self.vel_y = 0 
-        # Verificar si esta encima del suelo
-        elif self.vel_y >= 0:
-          dy = tile[1].top - self.rect.bottom
-          self.vel_y = 0 
+      # Verificar colision
+      for tile in world.tile_list: 
+        # Colision en X
+        if tile[1].colliderect(self.rect.x + dx, self.rect.y, self.width, self.height):
+          dx = 0
+        
+        # Si colisiona con algo en Y 
+        if tile[1].colliderect(self.rect.x, self.rect.y + dy, self.width, self.height):
+          # Verificar si esta debajo del suelo 
+          if self.vel_y < 0: 
+            dy = tile[1].bottom - self.rect.top
+            self.vel_y = 0 
+          # Verificar si esta encima del suelo
+          elif self.vel_y >= 0:
+            dy = tile[1].top - self.rect.bottom
+            self.vel_y = 0 
+        
+      # Si nuestro objeto Player colisiona con el enemigo
+      if pygame.sprite.spritecollide(self, blob_group, False):
+        game_over = -1
       
-    # Actualizar coordenadas jugador 
-    self.rect.x += dx 
-    self.rect.y += dy 
-    
-    # rect.bottom es igual a la maxima altura de la pantalla
-    # hacer que dy igual a 0 
-    # porque 
-    if self.rect.bottom > screen_height: 
-      self.rect.bottom = screen_height
-      dy = 0 
-    
+      if pygame.sprite.spritecollide(self, lava_group, False):
+        game_over = -1
+        
+      # Actualizar coordenadas jugador 
+      self.rect.x += dx 
+      self.rect.y += dy 
+      
+      # rect.bottom es igual a la maxima altura de la pantalla
+      # hacer que dy igual a 0 
+      # porque 
+      
+    # elif game_over == -1:
+    #   self.image = self.dead_image
+    #   if self.rect.y > 200:
+    #     self.rect.y -= 5
+      
     screen.blit(self.image, self.rect)
     pygame.draw.rect(screen, (255,255,255), self.rect, 2)
+    return game_over
        
 class World():
   def __init__(self, data):
@@ -157,15 +170,20 @@ class World():
           tile = (img, img_rect)
           self.tile_list.append(tile)
         if tile == 3:
+          # Aca acomodo la posicion y de mi enemigo
           blob = Enemy(col_count * tile_size, row_count * tile_size - 5)
           blob_group.add(blob)
         col_count += 1
+        if tile == 6:
+          lava = Lava(col_count * tile_size, row_count * tile_size) 
+          lava_group.add(lava)
+        
       row_count += 1
     
   def draw(self): 
     for tile in self.tile_list:
       screen.blit(tile[0],tile[1])
-      # pygame.draw.rect(screen, (255,255,255), tile[1], 2)
+      pygame.draw.rect(screen, (255,255,255), tile[1], 2)
     
 class Enemy(pygame.sprite.Sprite): 
   # Esta clase donde hacemos herencia ya tiene metodo: draw y update
@@ -188,7 +206,14 @@ class Enemy(pygame.sprite.Sprite):
       self.move_direction *= -1 
       self.move_counter *= -1      
     
-
+class Lava(pygame.sprite.Sprite): 
+  def __init__(self,x,y):
+    pygame.sprite.Sprite.__init__(self)
+    img = pygame.image.load('img/lava.png')
+    self.image = pygame.transform.scale(img, (tile_size, tile_size))
+    self.rect = self.image.get_rect()
+    self.rect.x = x
+    self.rect.y = y
 
 
 # matriz con la informacion del juego
@@ -217,6 +242,7 @@ world_data = [
 
 player = Player(100, screen_height - 130)
 blob_group = pygame.sprite.Group()
+lava_group = pygame.sprite.Group()
 world = World(world_data)
 # Why do I create this blob group ? How to explain ?
 
@@ -227,7 +253,7 @@ run = True
 while run:
   # Insertando componentes del mapa del juego
   screen.blit(bg_img, (0,0))
-  screen.blit(sun_img, (100, 100))
+  # screen.blit(sun_img, (100, 100))
   
   clock.tick(fps)
   
@@ -236,13 +262,17 @@ while run:
   # Con este blob_group llamamos una funcion y esta la aplica a todos 
   # los enemigos, osea no usamos update por cada enemigo en nuestro nivel
   blob_group.update()
+  lava_group.draw(screen)
   blob_group.draw(screen)
-  player.update()
+  
+  game_over = player.update(game_over)
   
   # Evento cerrar juego
   for event in pygame.event.get():
     if event.type == pygame.QUIT: 
       run = False 
+      
+  player.update(game_over)
     
   pygame.display.update()
       
